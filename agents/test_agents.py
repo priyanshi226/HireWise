734712a3f535b0cd9467e14a8b5d86b4a57ce648
Priyanshi Gupta <priyanshigupta0303@gmail.com>
2025-04-10 20:16:04 +0530

    first commit

--- agents/test_agents.py
+++ agents/test_agents.py
@@ -3,13 +3,24 @@
 from jd_summarizer import summarize_jd
 from resume_extractor import extract_resume
 from matcher import calculate_match_score
+import fitz  # PyMuPDF
 
 # Load your sample data
 with open("../JD.txt", "r", encoding="ISO-8859-1") as f:
     jd_text = f.read()
 
-with open("../Resume.pdf", "r", encoding="ISO-8859-1") as f:
-    resume_text = f.read()
+# with open("../Resume.pdf", "r", encoding="ISO-8859-1") as f:
+#     resume_text = f.read()
+
+def extract_text_from_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+resume_text = extract_text_from_pdf("../Resume.pdf")
+
 
 # Step 1: Summarize JD
 jd_summary = summarize_jd(jd_text)
